0ba933d03f2a35c2a67d58c454be6a47b694e917
mutkin251 <mutkin@kse.org.ua>
2024-11-10 14:51:18 +0200

    Changed_code_bms

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
+from time import sleep
 print(" Додаткове завдання")
 achievements = {
-    'Mine to die': {"locked": "Die by digging down"},
-    'MINE to mine': {"locked": "Craft a pickaxe"},
-    'It HURT!': {"locked": "Die by falling down"}
+    'Mine to die': {
+        'status': "locked",
+        'description': "Die by digging down"
+    },
+    'Mine to mine': {
+        'status': "locked",
+        'description': "Craft a pickaxe"
+    },
+    'It HURT!': {
+        'status': "locked",
+        'description': "Die by falling down"
+    }
 }
-def display_achievments(achievments):
-    print(achievments)
-
-def add_achievment(achievments, name, description):
-    achievments[name] = description
-    print(achievments)
-def unlock_achievment(achievements, name, description):
-    achievements.pop(name)
-    achievements[name]={"unlocked", description}
+print(achievements.keys())
+def display_achievements(achievements):
     print(achievements)
-choice=int(input("What do you want to do? 1-display achievments; 2-unlock achievments; 3-add achievment; 4-exit; "))
-gip = True
-while gip==True:
+
+def add_achievement(achievements, name, description, status):
+    achievements[name] = [status, description]
+    print(achievements)
+def unlock_achievement(achievements, name,status):
+    if achievements[name].get('status')=="locked":
+        achievements[name].update({'status': "unlocked"})
+    else:
+        print("That achievement is unlocked; ")
+    print(achievements)
+def delete_achievement(achievements,name):
+    achievements.pop(name)
+    print(achievements)
+while 1>0:
+    sleep(4)
+    print("===="*100)
+    choice = int(input("What do you want to do? 1-display achievments(full_info); 2-unlock achievments; 3-add achievment; 4-delete achievment; 5-exit; "))
     if choice ==1:
-        display_achievments(achievements)
-        break
+        display_achievements(achievements)
     elif choice == 2:
-        unlock_achievment(achievements,input(f"Want to open an achievement? Which one? Print IT! "), input("New description of this achievments(It can be he same): "))
+        name = input(f"Want to open an achievement? Which one? Print IT! ")
+        unlock_achievement(achievements,name,achievements[name].get('status'))
     elif choice == 3:
-        add_achievment(achievements, input("Name a new achievement: "), input("Description of this new achievments: "))
+        add_achievement(achievements, input("Name a new achievement: "), input("Description of this new achievments: "), input("Status of the achievment(locked,unlocked): "))
     elif choice == 4:
-        gip=False
+        delete_achievement(achievements, input("Enter the name of the achievement that you want to delete: "))
+    elif choice == 5:
         break
